0f4919097f4cbd79692c26a04171097eefa755f3
dophamine <dophaminedev@gmail.com>
2016-07-01 11:16:36 +0300

    added routing

---
+++ www/app.config.js
@@ -0,0 +1,42 @@
+;
+(function () {
+
+  angular.module('carman')
+    .config(function ($stateProvider, $urlRouterProvider) {
+
+      $stateProvider
+        .state('tab', {
+          abstract: true,
+          url: '/tabs',
+          templateUrl: 'templates/tabs.html'
+        })
+        .state('tab.cars', {
+          url: '/cars',
+          views: {
+            'cars': {
+              templateUrl: 'templates/cars.html'
+            }
+          }
+        })
+        .state('tab.cars-edit', {
+          url: '/cars/:id',
+          views: {
+            'cars': {
+              templateUrl: 'templates/cars.edit.html'
+            }
+          }
+        })
+        .state('tab.cars-add', {
+          url: '/cars-add',
+          views: {
+            'cars-add': {
+              templateUrl: 'templates/cars.add.html'
+            }
+          }
+        })
+
+      $urlRouterProvider.otherwise('/tabs/cars');
+
+    });
+
+})();
